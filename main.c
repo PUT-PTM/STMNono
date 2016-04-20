47c6620a2ac5e35a8bc37afa6bd48d0360aa9464
Mateusz Korolow <mateuszkorolow@gmail.com>
2016-04-20 20:25:24 +0200

    Skonfigurowanie PWM umożliwiającego sterowanie obrotami silnika DC podłączonego przez DRV8835  do pierwszego z kanałów TIMER4 (PB6) za pomocą funkcji przyjmującej za parametr procentowy stosunek mocy 0-100%.

--- main.c
+++ main.c
@@ -3,36 +3,107 @@
 #include "stm32f4xx_gpio.h"
 #include "stm32f4xx_tim.h"
 
+void TM_PINS_Init(void)
+{
+	/*
+	 *
+	 *  TIM4 CH1 - PB6 / PD12
+	 *  TIM4 CH2 - PB7 / PD13
+	 *  TIM4 CH3 - PB8 / PD14
+	 *  TIM4 CH4 - PB9 / PD15
+	 *
+	 * */
+
+     GPIO_InitTypeDef GPIO_InitStruct;
+
+     /* POD£¥CZENIE ZEGARA DO GPIOB I GPIOD */
+
+     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+
+     /* USTAWIENIE PINOW JAKO WYJSC ANALOGOWYCH DLA PWM */
+
+     GPIO_PinAFConfig(GPIOB, GPIO_PinSource6, GPIO_AF_TIM4);
+     GPIO_PinAFConfig(GPIOB, GPIO_PinSource7, GPIO_AF_TIM4);
+     GPIO_PinAFConfig(GPIOB, GPIO_PinSource8, GPIO_AF_TIM4);
+     GPIO_PinAFConfig(GPIOB, GPIO_PinSource9, GPIO_AF_TIM4);
+
+     /* USTAWIENIE PINOW */
+
+     GPIO_InitStruct.GPIO_Pin = GPIO_Pin_6|GPIO_Pin_7|GPIO_Pin_8|GPIO_Pin_9;
+     GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+     GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+     GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+     GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+     GPIO_Init(GPIOB, &GPIO_InitStruct);
+}
+void TM_TIMER_Init(void)
+{
+    TIM_TimeBaseInitTypeDef TIM_BaseStruct;
+
+    /* POD£¥CZENIE ZEGARA DO TIMERA 4 */
+
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+
+    /* WYPE£NIENIE STRUKTURY INICJALIZACYJNEJ TIMERA 4 */
+
+    TIM_BaseStruct.TIM_Prescaler = 0;
+    TIM_BaseStruct.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM_BaseStruct.TIM_Period = 8399; /* 10kHz PWM */
+    TIM_BaseStruct.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM_BaseStruct.TIM_RepetitionCounter = 0;
+
+    TIM_TimeBaseInit(TIM4, &TIM_BaseStruct);
+
+    /* W£¥CZENIE TIMERA 4 */
+
+    TIM_Cmd(TIM4, ENABLE);
+}
+void TM_PWM_Init(float value)
+{
+    TIM_OCInitTypeDef TIM_OCStruct;
+
+    /* OGÓLNE USTAWIENIA */
+
+    TIM_OCStruct.TIM_OCMode = TIM_OCMode_PWM2;
+    TIM_OCStruct.TIM_OutputState = TIM_OutputState_Enable;
+    TIM_OCStruct.TIM_OCPolarity = TIM_OCPolarity_Low;
+
+    /* USTAWIENIA WYPE£NIENIA KANA£ÓW TIMERA */
+
+    /*
+    TIM_OCStruct.TIM_Pulse = 2099; // 25% CYKLU PRACY CH1 (PB6)
+    TIM_OC1Init(TIM4, &TIM_OCStruct);
+    TIM_OC1PreloadConfig(TIM4, TIM_OCPreload_Enable);
+
+    TIM_OCStruct.TIM_Pulse = 4199; // 50% CYKLU PRACY CH2 (PB7)
+    TIM_OC2Init(TIM4, &TIM_OCStruct);
+    TIM_OC2PreloadConfig(TIM4, TIM_OCPreload_Enable);
+
+    TIM_OCStruct.TIM_Pulse = 6299; // 75% CYKLU PRACY CH3 (PB8)
+    TIM_OC3Init(TIM4, &TIM_OCStruct);
+    TIM_OC3PreloadConfig(TIM4, TIM_OCPreload_Enable);
+
+    TIM_OCStruct.TIM_Pulse = 8399; // 100% CYKLU PRACY CH4 (PB9)
+    TIM_OC4Init(TIM4, &TIM_OCStruct);
+    TIM_OC4PreloadConfig(TIM4, TIM_OCPreload_Enable);
+    */
+
+    TIM_OCStruct.TIM_Pulse = (8400 * value)/100 - 1; // value% CYKLU PRACY CH1 (PB6)
+	TIM_OC1Init(TIM4, &TIM_OCStruct);
+	TIM_OC1PreloadConfig(TIM4, TIM_OCPreload_Enable);
+}
+
+
 int main(void)
 {
 	SystemInit();
 
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-
-	GPIO_InitTypeDef  GPIO_InitStructure;
-
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13| GPIO_Pin_14| GPIO_Pin_15;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-	GPIO_Init(GPIOD, &GPIO_InitStructure);
-
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
-
-	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
-
-	TIM_TimeBaseStructure.TIM_Period = 39;
-	TIM_TimeBaseStructure.TIM_Prescaler = 2099;
-	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
-	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
-	TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);
-	TIM_Cmd(TIM4, ENABLE);
-
+	TM_PINS_Init();
+	TM_TIMER_Init();
+	TM_PWM_Init(60);
 
 	while(1)
 	{
-
 	}
 
 }
